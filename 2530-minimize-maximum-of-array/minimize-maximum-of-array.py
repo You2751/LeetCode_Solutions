e9e637f99a4731f68ebfe46de00c733419b9c998
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-04-02 08:17:49 +0200

    Time: 594 ms (21.29%) | Memory: 27.8 MB (39.36%) - LeetSync

---
+++ 2530-minimize-maximum-of-array/minimize-maximum-of-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimizeArrayValue(self, nums: List[int]) -> int:
+        def can_reduce(mid):
+            carry = 0
+            for i in range(len(nums) - 1, 0 , -1):
+                total = nums[i] + carry
+                if(total > mid):
+                    carry = total - mid
+                else:
+                    carry = 0
+            return nums[0] + carry <= mid
+        left, right = 0, max(nums)
+        while(left <= right):
+            mid = (left + right) >> 1
+            if(can_reduce(mid)):
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
