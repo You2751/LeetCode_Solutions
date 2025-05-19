cf0dfe57da7b6edf19b46ac73be69aebecdb12d4
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-05-19 10:47:40 +0300

    Time: 1 ms (55.95%) | Memory: 17.8 MB (41.71%) - LeetSync

--- 3871-minimum-deletions-for-at-most-k-distinct-characters/minimum-deletions-for-at-most-k-distinct-characters.py
+++ 3871-minimum-deletions-for-at-most-k-distinct-characters/minimum-deletions-for-at-most-k-distinct-characters.py
@@ -3,11 +3,11 @@ class Solution:
         counter = Counter(s)
         if(len(counter) <= k):
             return 0
-        distinct_chars = len(counter)
-        deleted_chars = result = 0
+        deleted_count = 0
+        unique_chars = len(counter)
         counter = sorted(counter.items(), key = lambda x:x[1])
-        for c, val in counter:
-            deleted_chars += val
-            distinct_chars -= 1
-            if(distinct_chars <= k):
-                return deleted_chars
+        for key, val in counter:
+            unique_chars -= 1
+            deleted_count += val
+            if(unique_chars <= k):
+                return deleted_count
