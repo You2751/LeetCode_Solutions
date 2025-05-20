a54d353b107de217791dac715d833846511f6f87
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-05-20 13:57:12 +0300

    Time: 532 ms (19.10%) | Memory: 40.2 MB (5.74%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,18 +1,18 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        left = result = 0
         max_heap = []
         min_heap = []
+        left = result = 0
         for right, val in enumerate(nums):
             heapq.heappush(max_heap, (-val, right))
             heapq.heappush(min_heap, (val, right))
-            
-            while(-max_heap[0][0]- min_heap[0][0] > limit):
+
+            while(-max_heap[0][0] - min_heap[0][0] > limit):
                 left = min(max_heap[0][1], min_heap[0][1]) + 1
 
-                while(min_heap[0][1] < left):
-                    heapq.heappop(min_heap)
                 while(max_heap[0][1] < left):
                     heapq.heappop(max_heap)
+                while(min_heap[0][1] < left):
+                    heapq.heappop(min_heap)
             result = max(result, right - left + 1)
-        return result 
+        return result
