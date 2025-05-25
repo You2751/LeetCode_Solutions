31d656af23338bf3e429e772efe099172736dda6
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-05-25 09:45:25 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (64.53%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.py
@@ -1,13 +1,16 @@
 class Solution:
     def shortestBeautifulSubstring(self, s: str, k: int) -> str:
-        shortest_substring = ''
         left = right = count_ones = 0
+        shortest_subtring = ''
         while(right < len(s)):
             count_ones += s[right] == '1'
             while(count_ones > k or (left < right and s[left] == '0')):
                 count_ones -= s[left] == '1'
                 left += 1
             right += 1
-            while(count_ones == k and (not shortest_substring or right - left < len(shortest_substring) or (right - left == len(shortest_substring) and s[left:right] < shortest_substring))):
-                shortest_substring = s[left:right]
-        return shortest_substring
+            while(count_ones == k and
+            (not shortest_subtring) or
+            (right - left < len(shortest_subtring)) or
+            (right - left == len(shortest_subtring) and s[left:right] < shortest_subtring)):
+                shortest_subtring = s[left:right]
+        return shortest_subtring
