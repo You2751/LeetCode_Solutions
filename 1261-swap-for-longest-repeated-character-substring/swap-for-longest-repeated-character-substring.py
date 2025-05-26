fc6e4f22d586c9ad6b28b16bd2ea5668dbcf7320
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-05-26 08:12:11 +0300

    Time: 23 ms (47.65%) | Memory: 17.7 MB (98.24%) - LeetSync

--- 1261-swap-for-longest-repeated-character-substring/swap-for-longest-repeated-character-substring.py
+++ 1261-swap-for-longest-repeated-character-substring/swap-for-longest-repeated-character-substring.py
@@ -1,19 +1,18 @@
 class Solution:
     def maxRepOpt1(self, text: str) -> int:
-        left = max_length = 0
         counter = Counter(text)
+        left = max_length = 0
         while(left < len(text)):
             curr_idx = left
-            while(curr_idx < len(text) and text[left] == text[curr_idx]):
+            while(curr_idx < len(text) and text[curr_idx] == text[left]):
                 curr_idx += 1
-            left_block = curr_idx - left
+            left_chunk = curr_idx - left
             next_idx = curr_idx + 1
             while(next_idx < len(text) and text[next_idx] == text[left]):
                 next_idx += 1
-            right_block = next_idx - curr_idx - 1
-            max_possible = min(left_block + right_block + 1, counter[text[left]])
+            right_chunk = next_idx - curr_idx - 1
+            max_possible = min(counter[text[left]], left_chunk + right_chunk + 1)
             max_length = max(max_length, max_possible)
-            max_length = max(max_length, left_block)
+            max_length = max(max_length, left_chunk)
             left = curr_idx
-        return max_length
-            
+        return max_length 
