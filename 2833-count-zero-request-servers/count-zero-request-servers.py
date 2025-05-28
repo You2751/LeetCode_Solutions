ec47ce2c4e14e57e3c3f6198ce6e5c3a55cd1184
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-05-29 00:11:01 +0300

    Time: 219 ms (35.82%) | Memory: 71.3 MB (56.73%) - LeetSync

--- 2833-count-zero-request-servers/count-zero-request-servers.py
+++ 2833-count-zero-request-servers/count-zero-request-servers.py
@@ -1,19 +1,18 @@
 class Solution:
     def countServers(self, n: int, logs: List[List[int]], x: int, queries: List[int]) -> List[int]:
-        result = [0] * len(queries)
         counter = defaultdict(int)
-        active_servers = window_start = window_end = 0
-        logs.sort(key = lambda x:x[1])
-        indexed_queries = sorted([query_time, idx] for idx, query_time in enumerate(queries))
-
-        for query_time, idx in indexed_queries:
-            while(window_end < len(logs) and logs[window_end][1] <= query_time):
+        result = [0] * len(queries)
+        logs.sort(key = lambda x :x[1])
+        window_end = window_start = active_servers = 0
+        index_queries = sorted([query, idx] for idx, query in enumerate(queries))
+        for query, idx in index_queries:
+            while(window_end < len(logs) and logs[window_end][1] <= query):
                 server_idx = logs[window_end][0]
                 counter[server_idx] += 1
                 if(counter[server_idx] == 1):
                     active_servers += 1
                 window_end += 1
-            while(window_start < window_end and logs[window_start][1] < query_time - x):
+            while(window_start < window_end and logs[window_start][1] < query - x):
                 server_idx = logs[window_start][0]
                 counter[server_idx] -= 1
                 if(counter[server_idx] == 0):
