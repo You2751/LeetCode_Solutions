f49c2f67add5f42d90b44e5be4061ad12ecd3c92
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-06-10 11:19:53 +0300

    Time: 26 ms (43.33%) | Memory: 17.6 MB (98.19%) - LeetSync

--- 1873-longest-nice-substring/longest-nice-substring.py
+++ 1873-longest-nice-substring/longest-nice-substring.py
@@ -1,15 +1,15 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
-        def check_nice(string):
-            for c in string:
-                if(c.swapcase() not in string):
+        def check_word(word):
+            for ch in word:
+                if(ch.swapcase() not in word):
                     return False
             return True
         window_size = len(s)
         while(window_size):
             for i in range(len(s) - window_size + 1):
                 string = s[i:i + window_size]
-                if(check_nice(string)):
+                if(check_word(string)):
                     return string
             window_size -= 1
         return ""
