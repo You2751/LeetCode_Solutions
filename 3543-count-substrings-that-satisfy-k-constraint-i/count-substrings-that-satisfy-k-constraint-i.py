267a23819976a18da756e0384e2e857d95c16a9d
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-06-11 14:53:16 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (58.81%) - LeetSync

--- 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.py
+++ 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.py
@@ -1,16 +1,16 @@
 class Solution:
     def countKConstraintSubstrings(self, s: str, k: int) -> int:
         result = left = ones = zeros = 0
-        for right in range(len(s)):
-            if(s[right] == '1'):
-                ones += 1
-            elif(s[right] == '0'):
+        for right, ch in enumerate(s):
+            if(ch == '0'):
                 zeros += 1
+            elif(ch == '1'):
+                ones += 1
             while(ones > k and zeros > k):
-                if(s[left] == '1'):
-                    ones -= 1
-                elif(s[left] == '0'):
+                if(s[left] == '0'):
                     zeros -= 1
+                elif(s[left] == '1'):
+                    ones -= 1
                 left += 1
             result += right - left + 1
         return result
