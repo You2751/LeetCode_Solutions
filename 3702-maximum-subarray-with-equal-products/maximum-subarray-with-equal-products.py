2c6e73666682007cb20fca595e4c67609143c378
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-06-12 19:39:06 +0300

    Time: 39 ms (49.25%) | Memory: 17.9 MB (32.24%) - LeetSync

--- 3702-maximum-subarray-with-equal-products/maximum-subarray-with-equal-products.py
+++ 3702-maximum-subarray-with-equal-products/maximum-subarray-with-equal-products.py
@@ -1,20 +1,12 @@
 class Solution:
     def maxLength(self, nums: List[int]) -> int:
-        max_val = result = float('-inf')
-        overall_lcm = 1
-
-        for num in nums:
-            max_val = max(max_val, num)
-            overall_lcm = math.lcm(overall_lcm, num)
-        max_all = overall_lcm * max_val
-
-        for i in range(len(nums)):
-            prod, gcd, lcm = 1, 0, 1
-            for j in range(i, len(nums)):
-                prod *= nums[j]
-                gcd = math.gcd(gcd, nums[j])
-                lcm = math.lcm(lcm, nums[j])
+        result = float('-inf')
+        for left in range(len(nums)):
+            gcd, lcm, prod = 0, 1, 1
+            for right in range(left, len(nums)):
+                prod *= nums[right]
+                gcd = math.gcd(gcd, nums[right])
+                lcm = math.lcm(lcm, nums[right])
                 if(prod == lcm * gcd):
-                    result = max(result, j - i + 1)
-
+                    result = max(result, right - left + 1)
         return result
