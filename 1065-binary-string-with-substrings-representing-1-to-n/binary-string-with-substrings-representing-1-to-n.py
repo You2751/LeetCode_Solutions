c26645ea8472305fd5f4630071633eca07f214e0
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-06-24 11:28:56 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.90%) - LeetSync

--- 1065-binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.py
+++ 1065-binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.py
@@ -1,6 +1,7 @@
 class Solution:
     def queryString(self, s: str, n: int) -> bool:
-        
+        if(len(bin(n)[2:]) > len(s)):
+            return False
         for num in range(1, n + 1):
             binary = bin(num)[2:]
             if(binary not in s):
