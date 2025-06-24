c4d31f6e1df9b52687c6d94838947493cb312baa
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-06-25 01:57:14 +0300

    Time: 24 ms (64.22%) | Memory: 27.8 MB (95.75%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        result = left_sum = sum(cardPoints[:k])
+        left_sum = result = sum(cardPoints[:k])
         right_idx = len(cardPoints)
         right_sum = 0
         for idx in range(k - 1, -1, -1):
