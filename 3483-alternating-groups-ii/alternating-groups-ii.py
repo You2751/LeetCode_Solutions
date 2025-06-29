a2bac238be0d65b3d0390ef37f97cc4e4093519e
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-06-29 16:42:15 +0300

    Time: 752 ms (48.80%) | Memory: 21.1 MB (97.42%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.py
+++ 3483-alternating-groups-ii/alternating-groups-ii.py
@@ -1,11 +1,12 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
-        for i in range(k - 1):
-            colors.append(colors[i])
+        n = len(colors)
         left = result = 0
-        for right, color in enumerate(colors):
-            if(right > 0 and colors[right] == colors[right - 1]):
+        for right in range(n + k - 1):
+            if(right > 0 and colors[right % n] == colors[(right - 1) % n]):
                 left = right
-            if(right - left + 1 >= k):
+            if(right - left + 1 == k):
                 result += 1
+                left += 1
         return result
+        
