59c781d1d7289aa7ac11b32583ce4336c4efd189
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-06-29 12:17:19 +0300

    Time: 9 ms (92.34%) | Memory: 19.4 MB (76.05%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.py
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.py
@@ -1,13 +1,13 @@
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         window = minutes
-        total_satisfied = max_extra = extra_satisfied = 0
-        for right, customer in enumerate(customers):
-            if(not grumpy[right]):
-                total_satisfied += customer
+        total_customers = max_customers = extra_customers = 0
+        for idx, customer in enumerate(customers):
+            if(not grumpy[idx]):
+                total_customers += customer
             else:
-                extra_satisfied += customer
-            if(right >= window and grumpy[right - window] == 1):
-                extra_satisfied -= customers[right - window]
-            max_extra = max(max_extra, extra_satisfied)
-        return total_satisfied + max_extra
+                extra_customers += customer
+            if(idx >= window and grumpy[idx - window] == 1):
+                extra_customers -= customers[idx - window]
+            max_customers = max(max_customers, extra_customers)
+        return total_customers + max_customers
