0aee9cc198c7c8d4a2087097d34150ff56c69d35
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-07-02 00:03:23 +0300

    Time: 165 ms (5.44%) | Memory: 18 MB (75.51%) - LeetSync

--- 1261-swap-for-longest-repeated-character-substring/swap-for-longest-repeated-character-substring.py
+++ 1261-swap-for-longest-repeated-character-substring/swap-for-longest-repeated-character-substring.py
@@ -1,18 +1,15 @@
 class Solution:
     def maxRepOpt1(self, text: str) -> int:
+        res = 0
         counter = Counter(text)
-        left = max_length = 0
-        while(left < len(text)):
-            curr_idx = left
-            while(curr_idx < len(text) and text[curr_idx] == text[left]):
-                curr_idx += 1
-            left_chunk = curr_idx - left
-            next_idx = curr_idx + 1
-            while(next_idx < len(text) and text[next_idx] == text[left]):
-                next_idx += 1
-            right_chunk = next_idx - curr_idx - 1
-            max_possible = min(counter[text[left]], left_chunk + right_chunk + 1)
-            max_length = max(max_length, max_possible)
-            max_length = max(max_length, left_chunk)
-            left = curr_idx
-        return max_length 
+        for char in set(text):
+            left = diff = 0
+            for right in range(len(text)):
+                if(text[right] != char):
+                    diff += 1
+                while(diff > 1):
+                    if(text[left] != char):
+                        diff -= 1
+                    left += 1
+                res = max(res, min(right - left + 1, counter[char]))
+        return res
