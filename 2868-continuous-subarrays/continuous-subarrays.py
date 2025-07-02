6848174be995fcbe7ae7bae469edd98756d94273
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-07-02 17:36:22 +0300

    Time: 776 ms (30.83%) | Memory: 45 MB (5.16%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.py
+++ 2868-continuous-subarrays/continuous-subarrays.py
@@ -1,14 +1,16 @@
 class Solution:
     def continuousSubarrays(self, nums: List[int]) -> int:
-        counter = defaultdict(int)
-        left = right = result = 0
-        while(right < len(nums)):
-            counter[nums[right]] += 1
-            while(max(counter) - min(counter) > 2):
-                counter[nums[left]] -= 1
-                if(counter[nums[left]] == 0):
-                    del counter[nums[left]]
+        max_heap = []
+        min_heap = []
+        result = left = 0
+        for right, num in enumerate(nums):
+            heapq.heappush(max_heap, (-num, right))
+            heapq.heappush(min_heap, (num, right))
+            while(-max_heap[0][0] - min_heap[0][0] > 2):
                 left += 1
+                while(max_heap[0][1] < left):
+                    heapq.heappop(max_heap)
+                while(min_heap[0][1] < left):
+                    heapq.heappop(min_heap)
             result += right - left + 1
-            right += 1
         return result
