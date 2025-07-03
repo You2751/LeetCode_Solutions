45793d1cb9a62efb3698e5a2f9c1a1dbe1538535
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-07-03 15:03:43 +0300

    Time: 203 ms (71.80%) | Memory: 18.2 MB (50.91%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
@@ -1,13 +1,13 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
         counter = Counter(s)
-        if any(counter[char] < k for char in "abc"):
-            return -1 
-        max_length = start_idx = 0
-        for idx, ch in enumerate(s):
-            counter[ch] -= 1
-            while(counter[ch] < k):
-                counter[s[start_idx]] += 1
-                start_idx += 1
-            max_length = max(max_length, idx - start_idx + 1)
+        left = max_length = 0
+        if(any(counter[char] < k for char in 'abc')):
+            return -1
+        for right, char in enumerate(s):
+            counter[char] -= 1
+            while(counter[char] < k):
+                counter[s[left]] += 1
+                left += 1
+            max_length = max(max_length, right - left + 1)
         return len(s) - max_length
