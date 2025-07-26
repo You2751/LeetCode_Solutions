f1d61b451e2547914b725d01ed32286cd18b30b2
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-07-26 12:41:02 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (58.06%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -1,11 +1,12 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
         def check(string):
-            stack = []
-            for c in string:
-                if(stack and c == '#'):
-                    stack.pop()
-                elif(c != '#'):
-                    stack.append(c)
-            return "".join(stack)
-        return check(s) == check(t)
+            skip = 0
+            for c in string[::-1]:
+                if(c == '#'):
+                    skip += 1
+                elif(skip > 0):
+                    skip -= 1
+                else:
+                    yield c
+        return all(x == y for x, y in zip_longest(check(s), check(t)))
