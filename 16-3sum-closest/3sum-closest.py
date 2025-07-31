ec2809338e0418413f98ec031158117f41beab0d
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-07-31 21:17:18 +0300

    Time: 368 ms (30.13%) | Memory: 17.9 MB (49.20%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -7,13 +7,13 @@ class Solution:
                 continue
             left, right = idx + 1, len(nums) - 1
             while(left < right):
-                curr_sum = nums[idx] + nums[left] + nums[right]
-                if(curr_sum > target):
+                check_sum = nums[idx] + nums[left] + nums[right]
+                if(check_sum > target):
                     right -= 1
-                elif(curr_sum < target):
+                elif(check_sum < target):
                     left += 1
                 else:
-                    return curr_sum
-                if(abs(closest_sum - target) > abs(curr_sum - target)):
-                    closest_sum = curr_sum
+                    return check_sum
+                if(abs(check_sum - target) < abs(closest_sum - target)):
+                    closest_sum = check_sum
         return closest_sum
