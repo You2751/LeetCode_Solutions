124d6e692d87b19dce9fa4bf492a4dc9146ce715
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-03 14:11:05 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (33.47%) - LeetSync

--- 457-circular-array-loop/circular-array-loop.py
+++ 457-circular-array-loop/circular-array-loop.py
@@ -2,8 +2,8 @@ class Solution:
     def circularArrayLoop(self, nums: List[int]) -> bool:
         n = len(nums)
         def next_index(index):
-            return (index + nums[index]) % n
-        for i in range(n):
+            return (index + nums[index]) % len(nums)
+        for i in range(len(nums)):
             if(nums[i] == 0):
                 continue
             slow, fast = i, next_index(i)
