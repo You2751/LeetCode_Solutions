6964234e07a918d6dba2528160c226f7d7f848ac
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-04 12:44:13 +0300

    Time: 14 ms (18.12%) | Memory: 18 MB (35.26%) - LeetSync

--- 827-expressive-words/expressive-words.py
+++ 827-expressive-words/expressive-words.py
@@ -5,19 +5,16 @@ class Solution:
             while(ptr1 < len(original) and ptr2 < len(word)):
                 if(original[ptr1] != word[ptr2]):
                     return False
-                stretch_orginal_ptr1 = ptr1
-                while(ptr1 < len(original) and original[ptr1] == original[stretch_orginal_ptr1]):
+                original_ptr1 = ptr1
+                while(ptr1 < len(original) and original[ptr1] == original[original_ptr1]):
                     ptr1 += 1
-                stretch_original_length = ptr1 - stretch_orginal_ptr1
-                
-                stretch_word_ptr2 = ptr2
-                while(ptr2 < len(word) and word[stretch_word_ptr2] == word[ptr2]):
-                    ptr2 += 1
-                stretch_word_length = ptr2 - stretch_word_ptr2
+                stretch_original = ptr1 - original_ptr1
+                word_ptr2 = ptr2
+                while(ptr2 < len(word) and word[ptr2] == word[word_ptr2]):
+                    ptr2 +=1 
+                stretch_word = ptr2 - word_ptr2
 
-                if(stretch_word_length > stretch_original_length):
-                    return False
-                if(stretch_original_length < 3 and stretch_word_length != stretch_original_length):
+                if((stretch_word > stretch_original) or (stretch_original < 3 and stretch_original != stretch_word)):
                     return False
             return ptr1 == len(original) and ptr2 == len(word)
         return sum(can_stretch(s, word) for word in words)
