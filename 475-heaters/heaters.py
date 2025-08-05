9e92bcfa51b6c2e4345ce2c1f126db312d03b4f4
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-05 12:20:54 +0300

    Time: 475 ms (5.57%) | Memory: 20.5 MB (46.89%) - LeetSync

---
+++ 475-heaters/heaters.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses.sort()
+        heaters.sort()
+        def can_cover(radius):
+            heater_idx = house_idx = 0
+            while(house_idx < len(houses)):
+                if(heater_idx >= len(heaters)):
+                    return False
+                min_range = heaters[heater_idx] - radius
+                max_range = heaters[heater_idx] + radius
+
+                if(houses[house_idx] < min_range):
+                    return False
+                if(houses[house_idx] > max_range):
+                    heater_idx += 1
+                else:
+                    house_idx += 1
+            return True
+        left, right = 0, int(1e9)
+        while(left < right):
+            mid = (left + right) // 2
+            if(can_cover(mid)):
+                right = mid
+            else:
+                left = mid + 1
+        return left
