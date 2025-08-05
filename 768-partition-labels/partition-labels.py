bd4cbc2f0c761ced23b233a64d0a58dd195497e0
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-05 10:43:26 +0300

    Time: 3 ms (91.36%) | Memory: 17.9 MB (12.28%) - LeetSync

---
+++ 768-partition-labels/partition-labels.py
@@ -0,0 +1,13 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        result = []
+        dic = defaultdict(int)
+        for idx, char in enumerate(s):
+            dic[char] = idx
+        left = right = 0
+        for idx, char in enumerate(s):
+            right = max(right, dic[char])
+            if(idx == right):
+                result.append(right - left + 1)
+                left = right + 1
+        return result
