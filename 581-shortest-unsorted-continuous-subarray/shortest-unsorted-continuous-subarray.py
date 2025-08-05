54b928d548cfe6d0b6533a6037d4d5df97e9e164
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-05 09:46:38 +0300

    Time: 4 ms (91.87%) | Memory: 18.6 MB (74.08%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
@@ -1,18 +1,16 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
         left, right = 0, len(nums) - 1
-        while(left + 1 < len(nums) and nums[left] <= nums[left + 1]):
+        while(left + 1 < len(nums) and nums[left + 1] >= nums[left]):
             left += 1
         if(left == len(nums) - 1):
             return 0
-        while(right - 1 >= 0 and nums[right] >= nums[right - 1]):
+        while(right >= 0 and nums[right] >= nums[right - 1]):
             right -= 1
-
-        arr = nums[left:right + 1]
-        arr_min = min(arr)
-        arr_max = max(arr)
-        while(left - 1 >= 0 and nums[left - 1] > arr_min):
+        window_min = min(nums[left:right + 1])
+        window_max = max(nums[left:right + 1])
+        while(left - 1 >= 0 and nums[left - 1] > window_min):
             left -= 1
-        while(right + 1< len(nums) and nums[right + 1] < arr_max):
+        while(right + 1 < len(nums) and nums[right + 1] < window_max):
             right += 1
         return right - left + 1
