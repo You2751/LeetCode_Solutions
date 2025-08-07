92499c3b30d5ae2da557da9cd2271bbb1f482edb
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-07 20:29:48 +0300

    Time: 41 ms (64.92%) | Memory: 20.2 MB (77.96%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.py
+++ 853-most-profit-assigning-work/most-profit-assigning-work.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        worker.sort()
         jobs = sorted(zip(difficulty, profit))
-        worker = sorted(worker)
-        idx = profit = best = 0
+        idx = result = best = 0
         for ability in worker:
             while(idx < len(jobs) and ability >= jobs[idx][0]):
                 best = max(best, jobs[idx][1])
                 idx += 1
-            profit += best
-        return profit
+            result += best
+        return result
