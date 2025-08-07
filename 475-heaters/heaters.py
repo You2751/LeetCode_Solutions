67b0f89f4c0e950467c93b144865b5dda03fe9f7
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-07 21:18:17 +0300

    Time: 35 ms (77.66%) | Memory: 20.5 MB (81.43%) - LeetSync

--- 475-heaters/heaters.py
+++ 475-heaters/heaters.py
@@ -1,28 +1,13 @@
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
-        houses.sort()
         heaters.sort()
-        def can_heat(radius):
-            heater_idx = house_idx = 0
-            while(house_idx < len(houses)):
-                if(heater_idx >= len(heaters)):
-                    return False
-                min_range = heaters[heater_idx] - radius
-                max_range = heaters[heater_idx] + radius
-
-                if(houses[house_idx] < min_range):
-                    return False
-                if(houses[house_idx] > max_range):
-                    heater_idx += 1
-                else:
-                    house_idx += 1
-            return True
+        houses.sort()
+        idx = radius = 0
         
-        left, right = 0, int(1e9)
-        while(left < right):
-            mid = (left + right) // 2
-            if(can_heat(mid)):
-                right = mid
-            else:
-                left = mid + 1
-        return left
+        for house in houses:
+            while(idx  + 1< len(heaters) and abs(house - heaters[idx]) >= abs(house - heaters[idx + 1])):
+                idx += 1
+            distance = abs(house - heaters[idx])
+            radius = max(radius, distance)
+        return radius
+        
