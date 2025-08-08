e25457e0f96e43e171641fbe2f42f92717f5a4b0
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-08 10:10:59 +0300

    Time: 94 ms (41.32%) | Memory: 20.5 MB (46.03%) - LeetSync

--- 475-heaters/heaters.py
+++ 475-heaters/heaters.py
@@ -1,13 +1,22 @@
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        def bisect(heaters, target):
+            left, right = 0, len(heaters) - 1
+            while(left < right):
+                mid = (left + right) // 2
+                if(heaters[mid] >= target):
+                    right = mid
+                else:
+                    left = mid + 1
+            return left
         heaters.sort()
         radius = 0
-
         for house in houses:
-            idx = bisect.bisect_left(heaters, house)
-            left_distance = float('inf') if idx == 0 else abs(house - heaters[idx - 1])
-            right_distance = float('inf') if idx == len(heaters) else abs(house - heaters[idx])
-            
+            idx = bisect(heaters, house)
+            left_distance = float('inf') if idx == 0 else abs(heaters[idx - 1] - house)
+            right_distance = float('inf') if idx == len(heaters) else abs(heaters[idx] - house)
             distance = min(left_distance, right_distance)
             radius = max(radius, distance)
+        
         return radius
+                
