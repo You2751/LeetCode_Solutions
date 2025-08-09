700a7b8bcdcbce6cb00b0de498f1df68d48875a7
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-09 15:33:00 +0300

    Time: 267 ms (79.52%) | Memory: 31.8 MB (59.47%) - LeetSync

--- 3755-maximum-product-of-first-and-last-elements-of-a-subsequence/maximum-product-of-first-and-last-elements-of-a-subsequence.py
+++ 3755-maximum-product-of-first-and-last-elements-of-a-subsequence/maximum-product-of-first-and-last-elements-of-a-subsequence.py
@@ -2,12 +2,13 @@ class Solution:
     def maximumProduct(self, nums: List[int], m: int) -> int:
         min_val = float('inf')
         max_product = max_val = float('-inf')
-        
+
         for end in range(m - 1, len(nums)):
-            curr_val = nums[end]
+            end_val = nums[end]
             start_val = nums[end - (m - 1)]
-            min_val = min(min_val, start_val)
+
+            min_val = min(min_val , start_val)
             max_val = max(max_val, start_val)
 
-            max_product = max(max_product, curr_val * min_val, curr_val * max_val)
+            max_product = max(max_product, end_val * min_val, end_val * max_val)
         return max_product
