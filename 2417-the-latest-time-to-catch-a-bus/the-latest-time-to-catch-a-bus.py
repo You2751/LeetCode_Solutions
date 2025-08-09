8d6804bef52b4ca4d4976ecceaea3c9a291692da
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-09 12:19:13 +0300

    Time: 114 ms (25.62%) | Memory: 40.2 MB (52.31%) - LeetSync

--- 2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.py
+++ 2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.py
@@ -1,21 +1,21 @@
 class Solution:
     def latestTimeCatchTheBus(self, buses: List[int], passengers: List[int], capacity: int) -> int:
-        idx = last_board = 0
+        idx = latest_time = 0
         buses.sort()
         passengers.sort()
         for i, bus in enumerate(buses):
-            persons = 0
-            while(idx < len(passengers) and persons < capacity and passengers[idx] <= bus):
-                persons += 1
+            count = 0
+            while(idx < len(passengers) and count < capacity and passengers[idx] <= bus):
+                count += 1
                 idx += 1
             if(i == len(buses) - 1):
-                last_board = persons
-        passengers_set = set(passengers)
-        if(last_board < capacity):
+                latest_time = count
+
+        if(latest_time < capacity):
             time = buses[-1]
         else:
             time = passengers[idx - 1] - 1
-
+        passengers_set = set(passengers)
         while(time in passengers_set):
             time -= 1
         return time
