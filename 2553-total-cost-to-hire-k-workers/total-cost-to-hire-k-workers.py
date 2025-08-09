f549961bf067ce89749fce1a6310a689e8fb8094
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-09 18:07:33 +0300

    Time: 99 ms (85.57%) | Memory: 26.9 MB (98.38%) - LeetSync

--- 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
@@ -1,21 +1,22 @@
 class Solution:
     def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
-        total_cost = 0
+        total = 0
         heap_left = costs[:candidates]
         heap_right = costs[max(candidates, len(costs) - candidates):]
-        left_idx = candidates
-        right_idx = len(costs) - candidates - 1
         heapq.heapify(heap_left)
         heapq.heapify(heap_right)
+        left_idx = candidates
+        right_idx = len(costs) - candidates - 1
         for _ in range(k):
-            if(not heap_right or (heap_left and heap_left[0] <= heap_right[0])):
-                total_cost += heapq.heappop(heap_left)
+            condition = not heap_right or (heap_left and heap_left[0] <= heap_right[0])
+            if(condition):
+                total += heapq.heappop(heap_left)
                 if(left_idx <= right_idx):
                     heapq.heappush(heap_left, costs[left_idx])
                     left_idx += 1
             else:
-                total_cost += heapq.heappop(heap_right)
+                total += heapq.heappop(heap_right)
                 if(left_idx <= right_idx):
                     heapq.heappush(heap_right, costs[right_idx])
                     right_idx -= 1
-        return total_cost
+        return total
