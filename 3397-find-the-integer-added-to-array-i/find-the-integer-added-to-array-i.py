95845961a96fd17e458b5b3e0cadba3140fa49db
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-09 20:01:09 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (63.01%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.py
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.py
@@ -1,7 +1,3 @@
 class Solution:
     def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
-        counter = defaultdict(int)
-        for n1 in nums1:
-            for n2 in nums2:
-                counter[n2 - n1] += 1
-        return max(counter, key = counter.get)
+        return min(nums2) - min(nums1)
