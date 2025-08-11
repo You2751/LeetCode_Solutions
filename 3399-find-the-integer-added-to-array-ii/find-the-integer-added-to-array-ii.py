4fe5610e59d9a2bac1d4ce032a255ba7b2926563
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-12 01:17:33 +0300

    Time: 43 ms (97.50%) | Memory: 17.7 MB (65.63%) - LeetSync

---
+++ 3399-find-the-integer-added-to-array-ii/find-the-integer-added-to-array-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minimumAddedInteger(self, nums1: List[int], nums2: List[int]) -> int:
+        nums1.sort()
+        nums2.sort()
+        def is_valid_difference(x):
+            ptr1 = ptr2 = mis_matches = 0
+            while(ptr1 < len(nums1) and ptr2 < len(nums2)):
+                if(nums1[ptr1] + x == nums2[ptr2]):
+                    ptr1 += 1
+                    ptr2 += 1
+                else:
+                    ptr1 += 1
+                    mis_matches += 1
+            return mis_matches <= 2
+        result = float('inf')
+        for i in range(3):
+            diff = nums2[0] - nums1[i]
+            if(is_valid_difference(diff)):
+                result = min(result, diff)
+        return result
