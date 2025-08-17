862fe67ebd0458992d6b1d7c373d75c18e335504
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-17 13:31:21 +0300

    Time: 2 ms (24.19%) | Memory: 17.7 MB (59.38%) - LeetSync

--- 961-long-pressed-name/long-pressed-name.py
+++ 961-long-pressed-name/long-pressed-name.py
@@ -1,11 +1,16 @@
 class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
         ptr1 = ptr2 = 0
-        while(ptr2 < len(typed)):
-            if(ptr1 < len(name) and name[ptr1] == typed[ptr2]):
+        while(ptr1 < len(name) and ptr2 < len(typed)):
+            if(name[ptr1] == typed[ptr2]):
                 ptr1 += 1
                 ptr2 += 1
-            elif(ptr2 > 0 and typed[ptr2] == typed[ptr2 - 1]):
+            elif(name[ptr1] != typed[ptr2] and ptr2 > 0 and typed[ptr2] == typed[ptr2 - 1]):
+                ptr2 += 1
+            else:
+                return False
+        while(ptr2 < len(typed)):
+            if(typed[ptr2] == name[-1]):
                 ptr2 += 1
             else:
                 return False
