53bc4313539fd60d5760ca1012ccbe10619d4a01
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-17 11:36:34 +0300

    Time: 14 ms (27.01%) | Memory: 19.7 MB (23.09%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -5,10 +5,10 @@ class Solution:
         for idx in range(len(nums) - 1, -1, -1):
             left_val = nums[left] ** 2
             right_val = nums[right] ** 2
-            if(left_val > right_val):
-                result[idx] = left_val
-                left += 1
-            else:
+            if(left_val < right_val):
                 result[idx] = right_val
                 right -= 1
+            else:
+                result[idx] = left_val
+                left += 1
         return result
