9a5e923d6f0cda771322045f0f1508776c80aa70
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-17 11:06:33 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (81.87%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -1,11 +1,12 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        idx = -1
-        for index, char in enumerate(word):
-            if(char == ch):
-                idx = index
+        result = ''
+        ch_idx = 0
+        for idx, c in enumerate(word):
+            if(ch == c):
+                ch_idx == idx
+                result = word[:idx + 1]
                 break
-        if(idx == -1):
-            return word
-        result = word[:idx + 1][::-1] + word[idx + 1:]
-        return result
+        if(result != ''):          
+            return result[::-1] + word[idx + 1:]
+        return word
