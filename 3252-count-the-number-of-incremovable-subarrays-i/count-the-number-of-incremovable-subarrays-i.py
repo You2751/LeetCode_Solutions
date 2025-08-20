efcc351eac3e8c725077574889c0c9c7d6049404
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-20 14:11:53 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (83.00%) - LeetSync

--- 3252-count-the-number-of-incremovable-subarrays-i/count-the-number-of-incremovable-subarrays-i.py
+++ 3252-count-the-number-of-incremovable-subarrays-i/count-the-number-of-incremovable-subarrays-i.py
@@ -4,12 +4,11 @@ class Solution:
         n = len(nums)
         if(n == 0):
             return 0
-
         prefix_idx, suffix_idx = 0, n - 1
 
         while(prefix_idx + 1 < n and nums[prefix_idx + 1] > nums[prefix_idx]):
             prefix_idx += 1
-
+        
         if(prefix_idx == n - 1):
             return (n*(n + 1)) // 2
         
@@ -20,11 +19,10 @@ class Solution:
         result += n - suffix_idx + 1
 
         i = 0
-        j = suffix_idx 
+        j = suffix_idx
         while(i <= prefix_idx):
             while(j < n and nums[i] >= nums[j]):
                 j += 1
             result += n - j
             i += 1
         return result
-
