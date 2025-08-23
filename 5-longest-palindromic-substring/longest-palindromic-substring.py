c37f56641570b28ceb57be143f5bd16b9c4d33c7
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-23 23:12:17 +0300

    Time: 207 ms (94.18%) | Memory: 17.8 MB (59.10%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,22 +1,22 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if(len(s) < 2):
+        n = len(s)
+        if(n < 2):
             return s
         def check_string(left, right):
-            while(left >= 0 and right < len(s) and s[left] == s[right]):
+            while(left >= 0 and right < n and s[left] == s[right]):
                 left -= 1
                 right += 1
             return left + 1, right - 1
         best_left = best_right = 0
 
         for idx in range(len(s)):
-            left_odd, right_odd = check_string(idx, idx)
-            left_even, right_even = check_string(idx, idx + 1)
-
-            if(right_even - left_even > best_right - best_left):
-                best_right = right_even
-                best_left = left_even
-            if(right_odd - left_odd > best_right - best_left):
-                best_right = right_odd
-                best_left = left_odd
-        return s[best_left:best_right + 1]    
+            odd_left, odd_right = check_string(idx, idx)
+            even_left, even_right = check_string(idx, idx + 1)
+            if(odd_right - odd_left > best_right - best_left):
+                best_right = odd_right
+                best_left = odd_left
+            if(even_right - even_left > best_right - best_left):
+                best_right = even_right
+                best_left = even_left
+        return s[best_left:best_right + 1]
