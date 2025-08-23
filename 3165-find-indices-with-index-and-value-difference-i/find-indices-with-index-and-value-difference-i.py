bc4b7a47fc13e303c07b1607a3a7a338a5d1a5d8
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-23 13:36:39 +0300

    Time: 1 ms (55.69%) | Memory: 17.9 MB (37.85%) - LeetSync

--- 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.py
+++ 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.py
@@ -2,13 +2,13 @@ class Solution:
     def findIndices(self, nums: List[int], idx_diff: int, valueDifference: int) -> List[int]:
         min_idx = max_idx = 0
         for idx in range(idx_diff, len(nums)):
-            diff_idx = idx - idx_diff
-            if(nums[diff_idx] < nums[min_idx]):
-                min_idx = diff_idx
-            if(nums[diff_idx] > nums[max_idx]):
-                max_idx = diff_idx
+            diff = idx - idx_diff
+            if(nums[diff] < nums[min_idx]):
+                min_idx = diff
+            if(nums[diff] > nums[max_idx]):
+                max_idx = diff
             if(nums[idx] - nums[min_idx] >= valueDifference):
                 return [min_idx, idx]
             if(nums[max_idx] - nums[idx] >= valueDifference):
-                return [idx, max_idx] 
+                return [idx, max_idx]
         return [-1, -1]
