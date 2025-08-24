39c91f6078514a92f8635e08f09d5d30f20551e8
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-24 19:32:20 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (68.82%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,13 +1,5 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        result = ""
-
         arr = s.strip().split()
         arr.reverse()
-        print(arr)
-        for idx, word in enumerate(arr):
-            if(" " in word):
-                continue
-            else:
-                result += word + " "
-        return result[:len(result) - 1]
+        return " ".join(arr)
