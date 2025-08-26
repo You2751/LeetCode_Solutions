03c3ea40e95c23d8e974c609c8585f8dcadb0817
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-26 11:54:36 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.06%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -1,14 +1,21 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        version1 = version1.split('.')
-        version2 = version2.split('.')
-        for val1, val2 in zip_longest(version1, version2, fillvalue = "0"):
-            val1 = int(val1)
-            val2 = int(val2)
-            if(val1 == val2):
+        def check_integer(num):
+            result = []
+            num = num.split(".")
+            for n in num:
+                result += [int(n)]
+            return result 
+        v1 = check_integer(version1)
+        v2 = check_integer(version2)
+        length = max(len(v1), len(v2))
+        for i in range(length):
+            a = v1[i] if i < len(v1) else 0
+            b = v2[i] if i < len(v2) else 0
+            if(a == b):
                 continue
-            if(val1 > val2):
+            elif(a > b):
                 return 1
-            else:
+            elif(a < b):
                 return -1
         return 0
