734ae05fe30d125f229e4762f0c128228d82b63d
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-26 12:36:08 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (60.46%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -1,17 +1,19 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        def check_integer(num):
+        def check_number(num):
             result = []
-            num = num.split(".")
+            num = num.split('.') 
             for n in num:
-                result += [int(n)]
-            return result 
-        v1 = check_integer(version1)
-        v2 = check_integer(version2)
+                result.append(int(n))
+            return result
+        v1 = check_number(version1)
+        v2 = check_number(version2)
         length = max(len(v1), len(v2))
+        
         for i in range(length):
             a = v1[i] if i < len(v1) else 0
             b = v2[i] if i < len(v2) else 0
+            print(a < b)
             if(a == b):
                 continue
             elif(a > b):
