740f2306d0611baf65868a08de29a4afddd4faf5
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-08-27 12:26:06 +0300

    Time: 55 ms (61.21%) | Memory: 18 MB (14.25%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -3,12 +3,12 @@ class Solution:
         left, right = 1, int(sqrt(c))
         if(right * right == c):
             return True
-        while(left<= right):
+        while(left <= right):
             check_sum = left**2 + right**2
-            if(check_sum > c):
-                right -= 1
-            elif(check_sum < c):
-                left += 1
-            elif(check_sum == c):
+            if(check_sum == c):
                 return True
+            elif(check_sum > c):
+                right -= 1
+            else:
+                left += 1
         return False
