6e9f0acdd08db63e96a41e38e99b44cb7f3d7862
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-09-07 12:25:07 +0300

    Time: 354 ms (50.52%) | Memory: 17.8 MB (49.18%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -3,19 +3,17 @@ class Solution:
         result = 0
         n = len(nums)
         if(n < 3):
-            return 0
+            return result
         nums.sort()
         closest_sum = float('inf')
         for idx in range(n - 2):
-            if(idx > 0 and nums[idx] == nums[idx - 1]):
-                continue
             left, right = idx + 1, n - 1
             while(left < right):
                 check_sum = nums[idx] + nums[left] + nums[right]
                 if(check_sum == target):
                     return check_sum
                 elif(check_sum > target):
-                    right -= 1
+                    right -=  1
                 else:
                     left += 1
                 if(abs(check_sum - target) < abs(closest_sum - target)):
