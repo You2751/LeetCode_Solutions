f3a81d2f223c276511a49e0d7d56e94f3136053b
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-09-14 22:22:09 +0300

    Time: 25 ms (95.77%) | Memory: 20.5 MB (42.76%) - LeetSync

--- 475-heaters/heaters.py
+++ 475-heaters/heaters.py
@@ -2,10 +2,10 @@ class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         houses.sort()
         heaters.sort()
-        n = len(heaters)
         idx = radius = 0
+        n = len(heaters)
         for house in houses:
-            while(idx + 1 < n and abs(house - heaters[idx + 1]) <= abs(house - heaters[idx])):
+            while(idx + 1 < n and abs(house - heaters[idx]) >= abs(house - heaters[idx + 1])):
                 idx += 1
             distance = abs(house - heaters[idx])
             radius = max(radius, distance)
