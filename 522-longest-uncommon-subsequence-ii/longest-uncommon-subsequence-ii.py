5c802bd695c02d13bdc9d364ce49ac856b11c28a
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-09-17 18:51:14 +0300

    Time: 9 ms (8.17%) | Memory: 17.7 MB (87.92%) - LeetSync

--- 522-longest-uncommon-subsequence-ii/longest-uncommon-subsequence-ii.py
+++ 522-longest-uncommon-subsequence-ii/longest-uncommon-subsequence-ii.py
@@ -1,24 +1,22 @@
 class Solution:
     def findLUSlength(self, strs: List[str]) -> int:
-        def is_subsequence(template, word):
-            ptr1 = ptr2 = 0
-            while(ptr1 < len(template) and ptr2 < len(word)):
-                if(template[ptr1] == word[ptr2]):
-                    ptr1 += 1
-                    ptr2 += 1
-                else:
-                    ptr1 += 1
-            return ptr2 == len(word)
-        strs.sort(key = lambda x:-len(x))
-        for left in range(len(strs)):
-            word = strs[left]
-            common = False
-            for right in range(len(strs)):
-                if(right == left):
+        result = -1
+        for idx1, word1 in enumerate(strs):
+            subsequence_check = False
+            for idx2, word2 in enumerate(strs):
+                if(idx1 == idx2):
                     continue
-                if(is_subsequence(strs[right], word)):
-                    common = True
+                if(self.is_subsequence(word2, word1)):
+                    subsequence_check = True
                     break
-            if(not common):
-                return len(word)
-        return -1
+            if(not subsequence_check):
+                result = max(result, len(word1))
+        return result
+
+    def is_subsequence(self, template, word):
+        ptr1 = ptr2 = 0
+        while(ptr1 < len(template) and ptr2 < len(word)):
+            if(template[ptr1] == word[ptr2]):
+                ptr2 += 1
+            ptr1 += 1
+        return ptr2 == len(word)
