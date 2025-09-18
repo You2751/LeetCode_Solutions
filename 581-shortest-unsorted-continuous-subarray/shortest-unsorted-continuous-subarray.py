8ec546cf03c2cd8ba2d2f2a75708e8cfe3c4ee3f
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-09-18 18:12:36 +0300

    Time: 4 ms (90.24%) | Memory: 19 MB (13.14%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
@@ -4,21 +4,20 @@ class Solution:
         n = len(nums)
         while(left + 1 < n and nums[left + 1] >= nums[left]):
             left += 1
-
         if(left == n - 1):
             return 0
-
         right = n - 1
         while(right - 1 >= 0 and nums[right] >= nums[right - 1]):
             right -= 1
         
-        min_window = min(nums[left:right + 1])
-        max_window = max(nums[left:right + 1])
+        window_min = min(nums[left:right + 1])
+        window_max = max(nums[left:right + 1])
 
-        while(left - 1 >= 0 and nums[left - 1] > min_window):
+        while(left - 1 >= 0 and nums[left - 1] > window_min):
             left -= 1
         
-        while(right + 1 < n and nums[right + 1] < max_window):
+        while(right + 1 < n and nums[right + 1] < window_max):
             right += 1
         
         return right - left + 1
+        
