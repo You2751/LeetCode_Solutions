fa2f1cff3fc68604dc01e1fd7c245d6c48bc28c1
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-09-18 08:57:08 +0300

    Time: 7 ms (53.45%) | Memory: 19.1 MB (62.68%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
@@ -1,26 +1,12 @@
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        if(k < 0):
-            return 0
         result = 0
-        nums.sort()
-        n = len(nums)
-        left, right = 0, 1
-        while(right < n):
-            if(left == right):
-                right += 1
-                continue 
-            diff = nums[right] - nums[left]
-            if(diff < k):
-                right += 1
-            elif(diff > k):
-                left += 1
+        counter = Counter(nums)
+        for key, val in counter.items():
+            if(k == 0):
+                if(val > 1):
+                    result += 1
             else:
-                result += 1
-                left += 1
-                while(left < right and nums[left] == nums[left - 1]):
-                    left += 1
-                while(right < n and nums[right] == nums[right - 1]):
-                    right += 1
+                if(key + k in counter):
+                    result += 1
         return result
-
