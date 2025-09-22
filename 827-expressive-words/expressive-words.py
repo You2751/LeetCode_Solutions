16c4b89f72ba0c62573fa231f02a2bbdfb108cad
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-09-22 09:44:39 +0300

    Time: 10 ms (51.60%) | Memory: 17.8 MB (60.45%) - LeetSync

--- 827-expressive-words/expressive-words.py
+++ 827-expressive-words/expressive-words.py
@@ -5,6 +5,7 @@ class Solution:
             while(ptr1 < len(original) and ptr2 < len(word)):
                 if(original[ptr1] != word[ptr2]):
                     return False
+                
                 original_ptr = ptr1
                 while(ptr1 < len(original) and original[ptr1] == original[original_ptr]):
                     ptr1 += 1
@@ -13,9 +14,9 @@ class Solution:
                 word_ptr = ptr2
                 while(ptr2 < len(word) and word[ptr2] == word[word_ptr]):
                     ptr2 += 1
-
                 word_stretch = ptr2 - word_ptr
-                if(original_stretch < word_stretch or (original_stretch == 2 and word_stretch < original_stretch)):
+
+                if(original_stretch < word_stretch or (original_stretch == 2 and original_stretch > word_stretch)):
                     return False
             return ptr1 == len(original) and ptr2 == len(word)
         return sum(can_stretch(s, word) for word in words)
