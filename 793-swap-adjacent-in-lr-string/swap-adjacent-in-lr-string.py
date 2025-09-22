7c87ab1cef3b1682c6b3bfb41b1a65e07c75de12
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-09-22 15:03:30 +0300

    Time: 7 ms (76.79%) | Memory: 17.8 MB (88.94%) - LeetSync

--- 793-swap-adjacent-in-lr-string/swap-adjacent-in-lr-string.py
+++ 793-swap-adjacent-in-lr-string/swap-adjacent-in-lr-string.py
@@ -5,18 +5,21 @@ class Solution:
         while(True):
             while(ptr1 < n and start[ptr1] == 'X'):
                 ptr1 += 1
+            
             while(ptr2 < n and end[ptr2] == 'X'):
                 ptr2 += 1
             
             if(ptr1 >= n and ptr2 >= n):
                 return True
+            
             if(ptr1 >= n or ptr2 >= n or start[ptr1] != end[ptr2]):
                 return False
             
             if(start[ptr1] == 'L' and end[ptr2] == 'L' and ptr1 < ptr2):
                 return False
-
-            if(start[ptr1] == 'R' and end[ptr2] == 'R' and ptr1 > ptr2):
+            
+            if(start[ptr1] == 'R' and end[ptr2] == 'R'and ptr1 > ptr2):
                 return False
+                
             ptr1 += 1
             ptr2 += 1
