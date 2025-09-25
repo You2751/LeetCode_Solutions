3aaaace79ab8dc9489fa992d5b65ee915759de6e
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-09-25 11:30:05 +0300

    Time: 95 ms (90.29%) | Memory: 19.4 MB (92.27%) - LeetSync

--- 868-push-dominoes/push-dominoes.py
+++ 868-push-dominoes/push-dominoes.py
@@ -1,18 +1,9 @@
 class Solution:
     def pushDominoes(self, dominoes: str) -> str:
-        #case 1 --> L..L --> fill left
-        #case 2 --> R..R --> Fill Right
-        #Case 3 --> R..L --> Fill between them till middle
-        #Case 4 --> L..R --> Do nothing
-        #Case 5 --> . --> Do nothing
-
-        #Edge case 1 --> ....L, start with Left 
-        #Edge Case 2 --> R...., end with Right 
-
         arr = list(dominoes)
+        n = len(arr)
         prev_idx = -1
         prev_char = 'L'
-        n = len(dominoes)
         for i in range(n + 1):
             if(i == n):
                 curr_char = 'R'
@@ -22,16 +13,14 @@ class Solution:
                 continue
             if(curr_char == prev_char):
                 for j in range(prev_idx + 1, i):
-                    arr[j] = curr_char
-            elif(curr_char == 'L' and prev_char == 'R'):
+                    arr[j] = prev_char
+            elif(prev_char == 'R' and curr_char == 'L'):
                 left = prev_idx + 1
                 right = i - 1
                 while(left < right):
-                    arr[left] = 'R'
-                    arr[right] = 'L'
+                    arr[left] = "R"
+                    arr[right] = "L"
                     left += 1
                     right -= 1
             prev_idx, prev_char = i, curr_char
-
-        return ''.join(arr)
-        
+        return "".join(arr)
