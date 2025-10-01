08801a31d29c05de292cfcb19d4c07bf19e9b4b0
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-10-01 11:08:34 +0300

    Time: 3 ms (66.43%) | Memory: 18 MB (22.14%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.py
+++ 1009-pancake-sorting/pancake-sorting.py
@@ -1,18 +1,19 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
         n = len(arr)
-        flip_sequence = []
+        flips = []
+
         for curr_idx in range(n - 1, 0, -1):
             target_val = curr_idx + 1
             target_idx = curr_idx
-
+            
             while(target_idx > 0 and arr[target_idx] != target_val):
                 target_idx -= 1
             
             if(target_idx < curr_idx):
                 if(target_idx > 0):
-                    flip_sequence.append(target_idx + 1)
+                    flips.append(target_idx + 1)
                     arr[:target_idx + 1] = arr[:target_idx + 1][::-1]
-                flip_sequence.append(curr_idx + 1)
+                flips.append(curr_idx + 1)
                 arr[:curr_idx + 1] = arr[:curr_idx + 1][::-1]
-        return flip_sequence
+        return flips
