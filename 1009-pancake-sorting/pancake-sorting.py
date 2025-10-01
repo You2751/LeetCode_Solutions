8bbef36397472379c6bbcc9386ba2a883c306c5d
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-10-01 10:43:01 +0300

    Time: 3 ms (66.43%) | Memory: 18 MB (22.14%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.py
+++ 1009-pancake-sorting/pancake-sorting.py
@@ -2,22 +2,17 @@ class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
         n = len(arr)
         flip_sequence = []
-        def reverse(array, right):
-            left = 0
-            while(left < right):
-                arr[left], arr[right] = arr[right], arr[left]
-                left += 1
-                right -= 1
-        for curr_position in range(n - 1, 0 , -1):
-            target_value = curr_position + 1
-            target_idx = curr_position
-            while(target_idx > 0 and arr[target_idx] != target_value):
+        for curr_idx in range(n - 1, 0, -1):
+            target_val = curr_idx + 1
+            target_idx = curr_idx
+
+            while(target_idx > 0 and arr[target_idx] != target_val):
                 target_idx -= 1
-            if(target_idx < curr_position):
+            
+            if(target_idx < curr_idx):
                 if(target_idx > 0):
                     flip_sequence.append(target_idx + 1)
-                    reverse(arr, target_idx)
-                flip_sequence.append(curr_position + 1)
-                reverse(arr, curr_position)
-        
+                    arr[:target_idx + 1] = arr[:target_idx + 1][::-1]
+                flip_sequence.append(curr_idx + 1)
+                arr[:curr_idx + 1] = arr[:curr_idx + 1][::-1]
         return flip_sequence
