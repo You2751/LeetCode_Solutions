b3c24c08d38783e52d5a90b496b6c48e05219348
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-10-04 13:07:17 +0300

    Time: 157 ms (39.73%) | Memory: 42.3 MB (38.85%) - LeetSync

--- 901-advantage-shuffle/advantage-shuffle.py
+++ 901-advantage-shuffle/advantage-shuffle.py
@@ -1,15 +1,15 @@
 class Solution:
     def advantageCount(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums2_index = [[val, i] for i, val in enumerate(nums2)]
-        nums2_index.sort(key = lambda x:(x[0], x[1]))
         nums1.sort()
-        left, right  = 0, len(nums2) - 1 
-        result = [0] * len(nums1)
+        nums2_with_idx = [[num, idx] for idx, num in enumerate(nums2)]
+        result = [0] * len(nums2_with_idx)
+        nums2_with_idx.sort()
+        left, right = 0, len(nums1) - 1
         for num in nums1:
-            if(num > nums2_index[left][0]):
-                result[nums2_index[left][1]] = num
+            if(nums2_with_idx[left][0] < num):
+                result[nums2_with_idx[left][1]] = num
                 left += 1
             else:
-                result[nums2_index[right][1]] = num
+                result[nums2_with_idx[right][1]] = num
                 right -= 1
         return result
