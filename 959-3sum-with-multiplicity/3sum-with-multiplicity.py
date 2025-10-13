76dc111829357ee8ca27b3f9304681980d317e88
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-10-13 14:15:02 +0300

    Time: 1350 ms (33.82%) | Memory: 17.9 MB (93.12%) - LeetSync

--- 959-3sum-with-multiplicity/3sum-with-multiplicity.py
+++ 959-3sum-with-multiplicity/3sum-with-multiplicity.py
@@ -1,40 +1,34 @@
 class Solution:
-    def threeSumMulti(self, arr, target):
-        MOD = 10**9 + 7
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
         arr.sort()
+        mod = 10**9 + 7
+        result = 0
         n = len(arr)
-        ans = 0
-        
-        for i in range(n):
+        for i in range(n - 2):
             t = target - arr[i]
             left, right = i + 1, n - 1
-            
-            while left < right:
-                if arr[left] + arr[right] < t:
-                    left += 1
-                elif arr[left] + arr[right] > t:
+            while(left < right):
+                check_sum = arr[left] + arr[right]
+                if(check_sum > t):
                     right -= 1
+                elif(check_sum < t):
+                    left += 1
                 else:
-                    # arr[left] + arr[right] == t
-                    if arr[left] != arr[right]:
-                        # Count duplicates
-                        l_count = 1
-                        r_count = 1
-                        while left + 1 < right and arr[left] == arr[left + 1]:
-                            l_count += 1
+                    if(arr[left] != arr[right]):
+                        left_count = right_count = 1
+                        while(left + 1 < right and arr[left] == arr[left + 1]):
+                            left_count += 1
                             left += 1
-                        while right - 1 > left and arr[right] == arr[right - 1]:
-                            r_count += 1
+                        while(right - 1 > left and arr[right] == arr[right - 1]):
+                            right_count += 1
                             right -= 1
-                        ans += l_count * r_count
-                        ans %= MOD
+                        result += left_count * right_count
+                        result %= mod
                         left += 1
                         right -= 1
                     else:
-                        # arr[left] == arr[right], combination C(n,2)
                         count = right - left + 1
-                        ans += count * (count - 1) // 2
-                        ans %= MOD
-                        break  # all pairs are counted
-            
-        return ans % MOD
+                        result += (count * (count - 1)) // 2
+                        result %= mod
+                        break
+        return result
