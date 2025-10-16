ad369cb50ea3c68ae4d3b7ab9568cbf23a19ae97
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-10-16 08:51:48 +0300

    Time: 19 ms (92.91%) | Memory: 18 MB (97.87%) - LeetSync

--- 1739-split-two-strings-to-make-palindrome/split-two-strings-to-make-palindrome.py
+++ 1739-split-two-strings-to-make-palindrome/split-two-strings-to-make-palindrome.py
@@ -3,10 +3,7 @@ class Solution:
         return self.check(a, b) or self.check(b, a)
     def check(self, a, b):
         left, right = 0, len(b) - 1
-        while(left < right):
-            if(a[left] == b[right]):
-                left += 1
-                right -= 1
-            else:
-                break
+        while(left < right and a[left] == b[right]):
+            left += 1
+            right -= 1
         return a[left:right + 1] == a[left:right + 1][::-1] or b[left:right + 1] == b[left:right + 1][::-1]
