bc1d35be847bd84ae8abded2686bcd42697a4a31
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-10-16 09:40:30 +0300

    Time: 121 ms (81.88%) | Memory: 17.9 MB (87.25%) - LeetSync

---
+++ 1699-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numTriplets(self, nums1: List[int], nums2: List[int]) -> int:
+        nums1.sort()
+        nums2.sort()
+        def check(nums1, nums2):
+            result = 0
+            counter = Counter(nums2)
+            for num in nums1:
+                target = num ** 2
+                left, right = 0, len(nums2) - 1
+                while(left < right):
+                    check_val = nums2[left] * nums2[right]
+                    if(check_val > target):
+                        right -= 1
+                    elif(check_val < target):
+                        left += 1
+                    else:
+                        if(nums2[left] == nums2[right]):
+                            n = counter[nums2[left]]
+                            result += (n * (n - 1)) // 2
+                            break
+                        else:
+                            result += counter[nums2[left]] * counter[nums2[right]]
+                            check_left = nums2[left]
+                            check_right = nums2[right]
+                        while(left <right and nums2[left] == check_left):
+                            left += 1
+                        while(left < right and nums2[right] == check_right):
+                            right -= 1
+            return result
+        return check(nums1, nums2) + check(nums2, nums1)
