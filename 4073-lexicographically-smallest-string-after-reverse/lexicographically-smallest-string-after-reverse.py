75fe056cbf79feaadc6bcb83439b060a31535e73
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2025-11-01 12:29:01 +0200

    Time: 102 ms (22.03%) | Memory: 17.8 MB (84.48%) - LeetSync

--- 4073-lexicographically-smallest-string-after-reverse/lexicographically-smallest-string-after-reverse.py
+++ 4073-lexicographically-smallest-string-after-reverse/lexicographically-smallest-string-after-reverse.py
@@ -1,8 +1,8 @@
 class Solution:
     def lexSmallest(self, s: str) -> str:
-        result = s
+        result = min(s, s[::-1])
         n = len(s)
-        for k in range(1, n + 1):
+        for k in range(1, n):
             reverse_start = s[:k][::-1] + s[k:]
             reverse_end = s[:n - k] + s[n-k:][::-1]
             result = min(result, reverse_start,reverse_end)
