cb3d46351d8948a0c080af79d224e34dc2760ad1
Youssef Hassan <89577812+You2751@users.noreply.github.com>
2026-08-31 16:21:00 +0300

    Time: 12 ms (28.58%) | Memory: 19.9 MB (14.55%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        result = []
+        nums = []
+        for arr in grid:
+            for num in arr:
+                nums.append(num)
+        print(nums)
+        for num in nums:
+            idx = abs(num) - 1
+            if(nums[idx] > 0):
+                nums[idx] = -nums[idx]
+            else:
+                result.append(idx + 1)
+        return result + [i + 1 for i, num in enumerate(nums) if num > 0]
